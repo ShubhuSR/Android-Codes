f1d8fbc82c0c0d84139e1228eb9ea5be2afa8f67
Shubham Rane <36849515+ShubhuSR@users.noreply.github.com>
2020-06-11 10:59:33 +0530

    Firebase Login

---
+++ FirebaseLoginDemo/app/src/main/java/com/shubh/firebaselogindemo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.shubh.firebaselogindemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
