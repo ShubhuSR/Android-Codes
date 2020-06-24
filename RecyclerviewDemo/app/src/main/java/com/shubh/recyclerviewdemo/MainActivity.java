ec9d139e6256355ca9e2ea732a1b9a6f48e5543c
Shubham Rane <shubhrane4896@gmail.com>
2020-06-24 12:25:52 +0530

    App Added

--- RecyclerviewDemo/app/src/main/java/com/shubh/recyclerviewdemo/MainActivity.java
+++ RecyclerviewDemo/app/src/main/java/com/shubh/recyclerviewdemo/MainActivity.java
@@ -10,9 +10,10 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements MyRecyclerViewAdapter.ItemClickListener {
+public class MainActivity extends AppCompatActivity {
 
     MyRecyclerViewAdapter adapter;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,15 +28,19 @@ public class MainActivity extends AppCompatActivity implements MyRecyclerViewAda
         animalNames.add("Goat");
 
         // set up the RecyclerView
-        RecyclerView recyclerView = findViewById(R.id.rvAnimals);
+        recyclerView = findViewById(R.id.rvAnimals);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         adapter = new MyRecyclerViewAdapter(this, animalNames);
-        adapter.setClickListener(this);
-        recyclerView.setAdapter(adapter);
-    }
 
-    @Override
-    public void onItemClick(View view, int position) {
-        Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
+        if (recyclerView != null) {
+            recyclerView.setAdapter(adapter);
+        }
+
+        adapter.setClickListener(new MyRecyclerViewAdapter.ItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                Toast.makeText(getApplicationContext(), "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
