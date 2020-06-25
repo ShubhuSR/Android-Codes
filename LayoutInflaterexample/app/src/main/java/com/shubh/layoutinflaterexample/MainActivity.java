addb986ff2eaf4d18b26da45f4507d34c2d5bd5a
Shubham Rane <shubhrane4896@gmail.com>
2020-06-25 16:36:43 +0530

    Updation

--- LayoutInflaterexample/app/src/main/java/com/shubh/layoutinflaterexample/MainActivity.java
+++ LayoutInflaterexample/app/src/main/java/com/shubh/layoutinflaterexample/MainActivity.java
@@ -3,12 +3,28 @@ package com.shubh.layoutinflaterexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    LinearLayout lmparent;
+
+    LayoutInflater layoutInflater;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lmparent=findViewById(R.id.mparent);
+        layoutInflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+
+        View myview=layoutInflater.inflate(R.layout.myrow, null,false);
+
+        lmparent.addView(myview);
+
+
     }
 }
